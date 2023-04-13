92291985557d13c94f6dfa558d0ac8fd5fad5b92
samsiljee <samsiljee@gmail.com>
2023-04-14 08:45:57 +1200

    Improved processing tab

--- MS_proteomics_shiny/app.R
+++ MS_proteomics_shiny/app.R
@@ -1,10 +1,8 @@
 # Shiny app to run proteomics analysis in R
-# Created by Sam Siljee - (c) 2023
+# Written by Sam Siljee - (c) 2023
 # Created 04/04/2023
 
 # Packages ----
-
-# Load required packages
 
 library(shiny)
 library(MSstats)
@@ -12,11 +10,9 @@ library(tidyverse)
 
 # UI ----
 
-# Display the UI, with tabs for each section
-
 ui <- navbarPage(
   title = "MS analysis",
-                 
+
 # Instructions ----
 
   tabPanel("Instructions",
@@ -107,9 +103,9 @@ tabPanel("Format", "Format and pre-filter your data to work in MSstats, options 
             "I still need to add functionality to download as .rda, .csv, or .tsv format, and a way to save the log file.",
     dataTableOutput("MSstats_input_tab"))),
 
-# Processing ----
+# Process ----
 
-tabPanel("Processing",
+tabPanel("Process",
   sidebarPanel(h4("MSstats processing options"),
     radioButtons("logTrans",
       "Base of log transformation",
@@ -119,47 +115,55 @@ tabPanel("Processing",
       "Normalisation method used to remove bias between runs",
       choiceNames = c("Equalize medians", "Quantile", "Global standards", "None"),
       choiceValues = c("equalizeMedians", "quantile", "globalStandards", FALSE)),
+    textInput("nameStandards",
+      "Named vector for global standard peptides (Global standards"),
     radioButtons("featureSubset",
       "Subset features to use",
       choiceNames = c("All", "Top 3", "Top N", "High quality"),
       choiceValues = c("all", "top3", "topN", "highQuality")),
     numericInput("n_top_feature",
-      "Number of top features to use for subsetting",
+      "Number of top features to use (Top N)",
       value = 3),
+    checkboxInput("remove_uninformative_feature_outlier",
+      "Remove noisy features and outliers before run-level summarisation (High quality)",
+      value = FALSE),
+    numericInput("min_feature_count",
+      "Minimum features required to be considered in feature selection algorythm (High quality)",
+      value = 2),
     radioButtons("summaryMethod",
       "Method used to summarise features",
       choiceNames = c("Tukey's median polish", "Linear mixed model"),
       choiceValues = c("TMP", "linear")),
+    checkboxInput("equalFeatureVar",
+      "Account for heterogeneous variation among intensities from different features (Linear)",
+      value = TRUE),
+    checkboxInput("MBimpute",
+      "Impute censored values by Accelated failure model (TMP)",
+      value = TRUE),
+    checkboxInput("remove50missing",
+      "Remove runs with >50% missing values (TMP)",
+      value = FALSE),
+    numericInput("maxQuantileforCensored",
+      "Maximum quantile for deciding censored missing values",
+      value = 0.999),
     actionButton("go_process", "Process!")),
   
   mainPanel("This section will be where MSstats processing happens. There will be drop down options here too for the settings.",
-            "Note that currently I've not got the \"Global standards\" method working as it takes a named vector as input"
+            "Note that currently I've not got the \"Global standards\" method working as it takes a named vector as input",
+            "Overview of processed protein level data",
+            dataTableOutput("MSstats_processed_protein_tab")
             )),
 
-# MSstats ----
+# Comparison ----
 
-  tabPanel("MSstats",
-    
-    "This section will be where MSstats is computed. There will be drop down options here too for the settings."
-    
-    ),
+  tabPanel("Comparison",
+    "This section will be where MSstats is computed. There will be drop down options here too for the settings."),
 
-# Comparisons ----
-
-  tabPanel("Comparisons",
-    
-    "This section will be where the comparisons will be defined. There will be an input panel to set up the comparion matrix"
-    
-    ),
-
-# Visualisations ----
+# Visualisation ----
 
   tabPanel("Visualisation",
-    
-    "This section will be for making the graphs. Again a sidebar panel to select the types of graphs."
-    
-    )
-
+    "This section will be for making the graphs. Again a sidebar panel to select the types of graphs.")
+# Close UI
 )
 
 # Server ----
@@ -227,36 +231,38 @@ server <- function(input, output, session){
     MSstats_input()
   })
 
-# Processing ----
+# Process ----
   # Reactive values
-  output$featureSubset <- reactive(input$featureSubset)
   
   MSstats_processed <- eventReactive(input$go_process, {
     dataProcess(
       MSstats_input(),
-      logTrans = 2,
-      normalization = "equalizeMedians",
-      nameStandards = NULL,
-      featureSubset = "all",
-      remove_uninformative_feature_outlier = FALSE,
-      min_feature_count = 2,
-      n_top_feature = 3,
-      summaryMethod = "TMP",
-      equalFeatureVar = TRUE,
-      censoredInt = "NA",
-      MBimpute = TRUE,
-      remove50missing = FALSE,
-      fix_missing = NULL,
-      maxQuantileforCensored = 0.999,
-      use_log_file = TRUE,
-      append = FALSE,
-      verbose = TRUE,
-      log_file_path = NULL
-    )
+      logTrans = as.numeric(input$logTrans),
+      normalization = input$normalization,
+      nameStandards = input$nameStandards,
+      featureSubset = input$featureSubset,
+      remove_uninformative_feature_outlier = input$remove_uninformative_feature_outlier,
+      min_feature_count = input$min_feature_count,
+      n_top_feature = input$n_top_feature,
+      summaryMethod = input$summaryMethod,
+      equalFeatureVar = input$equalFeatureVar,
+      censoredInt = input$censoredInt,
+      MBimpute = input$MBimpute,
+      remove50missing = input$remove50missing,
+      fix_missing = input$fix_missing,
+      maxQuantileforCensored = input$maxQuantileforCensored,
+      use_log_file = FALSE)
   })
   
   # Output
-    
+  output$MSstats_processed_protein_tab <- renderDataTable({
+    MSstats_processed()$ProteinLevelData
+  })
+ 
+# Comparison ----
+  
+# Visualisation ----
+     
 # Testing ----
   
   # Close the server function
