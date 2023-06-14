cc82a5161c812209f827bebd5386ab949d316209
samsiljee <samsiljee@gmail.com>
2023-06-15 09:05:08 +1200

    Minor correction

--- MS_proteomics_shiny/server.R
+++ MS_proteomics_shiny/server.R
@@ -423,7 +423,7 @@ STRING_enrichment <- eventReactive(input$go_STRING, {
 # STRING annotations
 
 # STRING clustering
-STRING_clustersList <- eventReactive(input$go_STRING, {
+STRING_clusters <- eventReactive(input$go_STRING, {
   if(input$cluster_STRING) {
     string_db()$get_clusters(
       STRING_dataset()$STRING_id,
