53cd949071903ff90138ebab0ea8b1acf2b23970
samsiljee <samsiljee@gmail.com>
2023-10-21 14:36:12 +1300

    Added separate file for TMT wizard annotations UI

--- MS_proteomics_shiny/TMTAnnotationWizardServer.R
+++ MS_proteomics_shiny/TMTAnnotationWizardServer.R
@@ -1,3 +1,5 @@
+# Variables ----
+
 # Initialise data frames to store wizard data - initialised outside of the modal to make it globally available
 wizard_runs_data <- reactiveVal(NULL)
 wizard_channels_data <- reactiveVal(NULL)
@@ -94,19 +96,19 @@ observeEvent(input$launch_wizard, {
       })
 
       # Render the channel level data frame as a DataTable
-      output$wizard_channels_table <- DT::renderDataTable({
-        datatable(
-          wizard_channels_data(),
-          options = list(
-            dom = "t",
-            paging = FALSE,
-            ordering = FALSE
-          ),
-          rownames = FALSE,
-          editable = TRUE,
-          class = "cell-border stripe"
-        )
-      })
+      # output$wizard_channels_table <- DT::renderDataTable({
+      #   datatable(
+      #     wizard_channels_data(),
+      #     options = list(
+      #       dom = "t",
+      #       paging = FALSE,
+      #       ordering = FALSE
+      #     ),
+      #     rownames = FALSE,
+      #     editable = TRUE,
+      #     class = "cell-border stripe"
+      #   )
+      # })
 
       # Variable for selected rows, for runs and channels
       runs_selected_rows <- reactiveVal(NULL)
@@ -114,10 +116,12 @@ observeEvent(input$launch_wizard, {
         runs_selected_rows(input$wizard_runs_table_rows_selected)
       })
       channels_selected_rows <- reactiveVal(NULL)
-      observe({
-        channels_selected_rows(input$wizard_channels_table_rows_selected)
-      })
+      # observe({
+      #   channels_selected_rows(input$wizard_channels_table_rows_selected)
+      # })
 
+      # Handlers for page data ----
+      
       # Handler to edit Run mixtures
       observeEvent(input$addMixture, {
         if (!is.null(runs_selected_rows())) {
@@ -240,142 +244,6 @@ observeEvent(input$launch_wizard, {
         })
       })
 
-      # Wizard "UI"----
-      # UI for the "Runs - Mixtures" page
-      wizard_mixtures_ui <- function() {
-        fluidPage(
-          h2("Runs: Mixtures"),
-          # Text entry for conditions
-          "Select one or more rows, and enter mixture below:",
-          DT::dataTableOutput("wizard_runs_table"),
-          numericInput("wizardMixture", "", value = 1),
-          actionButton("addMixture", "Add mixture"),
-
-          # Hide buttons
-          shinyjs::hide("backButton"),
-          shinyjs::hide("doneButton"),
-          shinyjs::hide("wizard_runs_annotations_tsv"),
-          # Show buttons
-          shinyjs::show("nextButton")
-        )
-      }
-
-      # UI for the "Runs - Fractions" page
-      wizard_fractions_ui <- function() {
-        fluidPage(
-          h2("Runs: Fractions"),
-          "Select one or more rows and enter fraction, or select \"Not fractionated\"",
-          DT::dataTableOutput("wizard_runs_table"),
-          checkboxInput("wizardFractionated", "Not fractionated", value = TRUE),
-          conditionalPanel(
-            condition = "input.wizardFractionated == false",
-            # Numeric entry for fraction
-            numericInput("wizardFraction", "", value = 1),
-            actionButton("addFraction", "Add fraction"),
-          ),
-
-          # Hide buttons
-          shinyjs::hide("doneButton"),
-          shinyjs::hide("wizard_runs_annotations_tsv"),
-          # Show buttons
-          shinyjs::show("nextButton"),
-          shinyjs::show("backButton")
-        )
-      }
-
-      # UI for the "Runs - TechRepMixture" page
-      wizard_techrepmixtures_ui <- function() {
-        fluidPage(
-          h2("Runs: Mixture Technical Replicates"),
-          "Select one or more rows and enter mixture technical replicate, or select \"No technical replicates\"",
-          DT::dataTableOutput("wizard_runs_table"),
-          checkboxInput("wizardReplicated", "No technical replicates", value = TRUE),
-          conditionalPanel(
-            condition = "input.wizardReplicated == false",
-            # Numeric entry for fraction
-            numericInput("wizardTechRepMixture", "", value = 1),
-            actionButton("addReplicate", "Add replicate"),
-          ),
-
-          # Hide buttons
-          shinyjs::hide("doneButton"),
-          # Show buttons
-          shinyjs::show("wizard_runs_annotations_tsv"),
-          shinyjs::show("nextButton"),
-          shinyjs::show("backButton")
-        )
-      }
-
-      # UI for the channels page
-      wizard_channels_ui <- function() {
-        fluidPage(
-          h2("Channels: Channels"),
-          "Select TMT plex, or enter custom channels",
-          conditionalPanel(
-            condition = "input.wizardCustomPlex == false",
-            selectInput("wizardPlexSelected",
-              "",
-              c(
-                "TMTPro 18-plex" = "TMTPro_18",
-                "TMTPro 16-plex" = "TMTPro_16",
-                "TMT 11-plex" = "TMT_11",
-                "TMT 10-plex" = "TMT_10",
-                "TMT 6-plex" = "TMT_6",
-                "TMT 2-plex" = "TMT_2"
-              ),
-              multiple = FALSE
-            )
-          ),
-          checkboxInput("wizardCustomPlex", "Custom channels", value = FALSE),
-          DT::dataTableOutput("wizard_channels_table"),
-
-          # Hide buttons
-          shinyjs::hide("doneButton"),
-          shinyjs::hide("wizard_runs_annotations_tsv"),
-          # Show buttons
-          shinyjs::show("nextButton"),
-          shinyjs::show("backButton")
-        )
-      }
-
-      # UI for the "Conditions" page
-      wizard_conditions_ui <- function() {
-        fluidPage(
-          h2("Conditions"),
-          # Text entry for conditions
-          "Select one or more rows, and enter condition below:",
-          DT::dataTableOutput("wizard_table"),
-          textInput("wizardCondition", "", ""),
-          actionButton("addCondition", "Add condition"),
-
-          # Hide "back" button on first page, and "next buttons from other pages.
-          shinyjs::hide("backButton"),
-          shinyjs::hide("nextButtonBioReplicates"),
-          shinyjs::hide("doneWizard"),
-          # Show conditions next button
-          shinyjs::show("nextButtonConditions")
-        )
-      }
-
-      # UI for the "BioReplicates" page
-      bio_replicates_wizard_ui <- function() {
-        fluidPage(
-          h2("Biological replicates"),
-          # Text entry for biological replicates
-          "Select one or more rows, and enter biological replicate below:",
-          DT::dataTableOutput("wizard_table"),
-          textInput("wizardBioReplicates", "", ""),
-          actionButton("addBioReplicate", "Add biological replicate"),
-
-          # Hide "next" buttons from other pages.
-          shinyjs::hide("nextButtonConditions"),
-          shinyjs::hide("doneWizard"),
-          # Show "back" and bioreplicates "next" button
-          shinyjs::show("nextButtonBioReplicates"),
-          shinyjs::show("backButton")
-        )
-      }
-
       # Launch wizard
       showModal(modalDialog(
         title = "Annotations wizard",
@@ -397,6 +265,6 @@ observeEvent(input$launch_wizard, {
         duration = NULL,
         closeButton = TRUE
       )
-    }
-  }
+    } # Close error pop-up if no raw data is uploaded
+  } # Close catch to only run for TMT epxeriments
 }) # Close launch wizard
---
+++ MS_proteomics_shiny/TMTAnnotationWizardUI.R
@@ -0,0 +1,129 @@
+# TMT annotation wizard UI elements
+
+# Runs ----
+wizard_mixtures_ui <- function() {
+    fluidPage(
+        h2("Runs: Mixtures"),
+        # Text entry for conditions
+        "Select one or more rows, and enter mixture below:",
+        DT::dataTableOutput("wizard_runs_table"),
+        numericInput("wizardMixture", "", value = 1),
+        actionButton("addMixture", "Add mixture"),
+        
+        # Hide buttons
+        shinyjs::hide("backButton"),
+        shinyjs::hide("doneButton"),
+        shinyjs::hide("wizard_runs_annotations_tsv"),
+        # Show buttons
+        shinyjs::show("nextButton")
+    )
+}
+
+wizard_fractions_ui <- function() {
+    fluidPage(
+        h2("Runs: Fractions"),
+        "Select one or more rows and enter fraction, or select \"Not fractionated\"",
+        DT::dataTableOutput("wizard_runs_table"),
+        checkboxInput("wizardFractionated", "Not fractionated", value = TRUE),
+        conditionalPanel(
+            condition = "input.wizardFractionated == false",
+            # Numeric entry for fraction
+            numericInput("wizardFraction", "", value = 1),
+            actionButton("addFraction", "Add fraction"),
+        ),
+        
+        # Hide buttons
+        shinyjs::hide("doneButton"),
+        shinyjs::hide("wizard_runs_annotations_tsv"),
+        # Show buttons
+        shinyjs::show("nextButton"),
+        shinyjs::show("backButton")
+    )
+}
+
+wizard_techrepmixtures_ui <- function() {
+    fluidPage(
+        h2("Runs: Mixture Technical Replicates"),
+        "Select one or more rows and enter mixture technical replicate, or select \"No technical replicates\"",
+        DT::dataTableOutput("wizard_runs_table"),
+        checkboxInput("wizardReplicated", "No technical replicates", value = TRUE),
+        conditionalPanel(
+            condition = "input.wizardReplicated == false",
+            # Numeric entry for fraction
+            numericInput("wizardTechRepMixture", "", value = 1),
+            actionButton("addReplicate", "Add replicate"),
+        ),
+        
+        # Hide buttons
+        shinyjs::hide("doneButton"),
+        # Show buttons
+        shinyjs::show("wizard_runs_annotations_tsv"),
+        shinyjs::show("nextButton"),
+        shinyjs::show("backButton")
+    )
+}
+
+# Channels ----
+
+wizard_channels_ui <- function() {
+    fluidPage(
+        h2("Channels: Channels"),
+        "Select TMT plex, or enter custom channels",
+        conditionalPanel(
+            condition = "input.wizardCustomPlex == false",
+            selectInput("wizardPlexSelected",
+                        "",
+                        c(
+                            "TMTPro 18-plex" = "TMTPro_18",
+                            "TMTPro 16-plex" = "TMTPro_16",
+                            "TMT 11-plex" = "TMT_11",
+                            "TMT 10-plex" = "TMT_10",
+                            "TMT 6-plex" = "TMT_6",
+                            "TMT 2-plex" = "TMT_2"
+                        ),
+                        multiple = FALSE
+            )
+        ),
+        checkboxInput("wizardCustomPlex", "Custom channels", value = FALSE),
+        # Hide buttons
+        shinyjs::hide("doneButton"),
+        shinyjs::hide("wizard_runs_annotations_tsv"),
+        # Show buttons
+        shinyjs::show("nextButton"),
+        shinyjs::show("backButton")
+    )
+}
+
+wizard_conditions_ui <- function() {
+    fluidPage(
+        h2("Conditions"),
+        # Text entry for conditions
+        "Select one or more rows, and enter condition below:",
+        textInput("wizardCondition", "", ""),
+        actionButton("addCondition", "Add condition"),
+        
+        # Hide "back" button on first page, and "next buttons from other pages.
+        shinyjs::hide("backButton"),
+        shinyjs::hide("nextButtonBioReplicates"),
+        shinyjs::hide("doneWizard"),
+        # Show conditions next button
+        shinyjs::show("nextButtonConditions")
+    )
+}
+
+wizard_bio_replicates_ui <- function() {
+    fluidPage(
+        h2("Biological replicates"),
+        # Text entry for biological replicates
+        "Select one or more rows, and enter biological replicate below:",
+        textInput("wizardBioReplicates", "", ""),
+        actionButton("addBioReplicate", "Add biological replicate"),
+        
+        # Hide "next" buttons from other pages.
+        shinyjs::hide("nextButtonConditions"),
+        shinyjs::hide("doneWizard"),
+        # Show "back" and bioreplicates "next" button
+        shinyjs::show("nextButtonBioReplicates"),
+        shinyjs::show("backButton")
+    )
+}
