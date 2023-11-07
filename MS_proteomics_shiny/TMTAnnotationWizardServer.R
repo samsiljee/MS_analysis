95c806c8a0f75f84a546dd4d1af3d2ce3eb0e33e
samsiljee <samsiljee@gmail.com>
2023-11-07 14:56:41 +1300

    TMT wizard annotation code grouped more sensibly

--- MS_proteomics_shiny/TMTAnnotationWizardServer.R
+++ MS_proteomics_shiny/TMTAnnotationWizardServer.R
@@ -14,13 +14,19 @@ TMT_Plexes <- list(
   TMT_2 = c("126", "127")
 )
 
-# Initialise data frames to store wizard data - initialised outside of the modal to make it globally available
+# Channels level server ----
+# Reactive variables
+# Initialise blank channels data frame
 wizard_channels_data <- reactiveVal(NULL)
-wizard_runs_data <- reactiveVal(NULL)
-
-# Initialise blank columns for channels data frame
+# Initialise blank columns
 wizard_conditions <- reactiveVal(NA)
 wizard_bioreplicates <- reactiveVal(NA)
+
+# Variable for selected channels rows
+channels_selected_rows <- reactiveVal(NULL)
+observe({
+  channels_selected_rows(input$wizard_channels_table_rows_selected)
+})
 
 # Read in channels
 wizard_channels <- reactive({
@@ -42,7 +48,7 @@ wizard_channels_mixtures <- reactive({
 first_two_columns <- reactive({
   channels <- wizard_channels()
   mixtures <- wizard_channels_mixtures()
-  
+
   expand.grid(Channel = channels, Mixture = mixtures)
 })
 
@@ -55,164 +61,153 @@ wizard_channels_data <- reactive({
   )
 })
 
+# Handler to edit Channel Conditions
+observeEvent(input$addCondition, {
+  if (!is.null(channels_selected_rows())) {
+    current_wizard_conditions <- wizard_conditions()
+    current_wizard_conditions[channels_selected_rows()] <- input$wizardCondition
+    wizard_conditions(current_wizard_conditions)
+  } else {
+    showNotification(
+      "Please select one or more rows first",
+      type = "error",
+      duration = NULL,
+      closeButton = TRUE
+    )
+  }
+})
+
+# Render the channel level data frame as a DataTable
+output$wizard_channels_table <- DT::renderDataTable({
+  datatable(
+    wizard_channels_data(),
+    options = list(
+      dom = "t",
+      paging = FALSE,
+      ordering = FALSE
+    ),
+    rownames = FALSE,
+    editable = TRUE,
+    class = "cell-border stripe"
+  )
+})
+
+# Runs level server ----
+# Reactive variables for the runs
+# Initialise data frame for run level wizard data
+wizard_runs_data <- reactiveVal(NULL)
+# Initialise blank columns for runs data frame
+wizard_runs_mixtures <- reactiveVal(NA)
+wizard_fractions <- reactiveVal(NA)
+wizard_techrepmixtures <- reactiveVal(NA)
+
+# Variable for selected rows, for runs and channels
+runs_selected_rows <- reactiveVal(NULL)
+observe({
+  runs_selected_rows(input$wizard_runs_table_rows_selected)
+})
+
+# Vector of unique runs from raw data
+wizard_runs <- reactive({
+  switch(input$platform,
+    PD = {
+      raw()$Run
+    },
+    MQ = {
+      if (length(raw()$Raw.file) != 0) {
+        raw()$Raw.file
+      } else {
+        raw()$`Raw file`
+      }
+    }
+  ) %>%
+    unique() %>%
+    sort()
+})
+
+# Handler to edit Fraction if adding fractions manually
+observeEvent(input$addFraction, {
+  if (!is.null(runs_selected_rows())) {
+    current_wizard_fractions <- wizard_fractions()
+    current_wizard_fractions[runs_selected_rows()] <- input$wizardFraction
+    wizard_fractions(current_wizard_fractions)
+  } else {
+    # Handle the case when no rows are selected
+    showNotification(
+      "Please select one or more rows first",
+      type = "error",
+      duration = NULL,
+      closeButton = TRUE
+    )
+  }
+})
+
+# Handler to edit Fraction if not fractionated
+observeEvent(input$wizardFractionated, {
+  if (input$wizardFractionated) {
+    wizard_fractions(1)
+  } else {
+    wizard_fractions(rep(NA, length(wizard_runs())))
+  }
+})
+
+# Handler to edit TechRepMixture if adding replicates manually
+observeEvent(input$addReplicate, {
+  if (!is.null(runs_selected_rows())) {
+    current_wizard_techrepmixtures <- wizard_techrepmixtures()
+    current_wizard_techrepmixtures[runs_selected_rows()] <- input$wizardTechRepMixture
+    wizard_techrepmixtures(current_wizard_techrepmixtures)
+  } else {
+    # Handle the case when no rows are selected
+    showNotification(
+      "Please select one or more rows first",
+      type = "error",
+      duration = NULL,
+      closeButton = TRUE
+    )
+  }
+})
+
+# Handler to edit TechRepMixture if not replicated
+observeEvent(input$wizardReplicated, {
+  if (input$wizardReplicated) {
+    wizard_techrepmixtures(1)
+  } else {
+    wizard_techrepmixtures(rep(NA, length(wizard_runs())))
+  }
+})
+
+# Update wizard_runs_data
+observe({
+  wizard_runs_data(
+    data.frame(
+      Fraction = wizard_fractions(),
+      TechRepMixture = wizard_techrepmixtures()
+    )
+  )
+})
+
+# Render the run level data frame as a DataTable
+output$wizard_runs_table <- DT::renderDataTable({
+  datatable(
+    wizard_runs_data(),
+    options = list(
+      dom = "t",
+      paging = FALSE,
+      ordering = FALSE
+    ),
+    rownames = FALSE,
+    editable = TRUE,
+    class = "cell-border stripe"
+  )
+})
+
+# Modal functions ----
 # Launch wizard
 observeEvent(input$launch_wizard, {
   if (input$quant_method == "TMT") { # Only run for TMT experiments
     if (nrow(raw()) != 0) { # only run wizard if the raw files are loaded
 
-      # Channels server ----
-     
-      # Create reactive variables for channels data
-      
-      
-      # Render the channel level data frame as a DataTable
-      output$wizard_channels_table <- DT::renderDataTable({
-        datatable(
-          wizard_channels_data(),
-          options = list(
-            dom = "t",
-            paging = FALSE,
-            ordering = FALSE
-          ),
-          rownames = FALSE,
-          editable = TRUE,
-          class = "cell-border stripe"
-        )
-      })
-      
-      # Handler to edit Channel Conditions
-      observeEvent(input$addCondition, {
-        if (!is.null(channels_selected_rows())) {
-          current_wizard_conditions <- wizard_conditions()
-          current_wizard_conditions[channels_selected_rows()] <- input$wizardCondition
-          wizard_conditions(current_wizard_conditions)
-        } else {
-          showNotification(
-            "Please select one or more rows first",
-            type = "error",
-            duration = NULL,
-            closeButton = TRUE
-          )
-        }
-      })
-      
-      # Variable for selected channels rows
-      channels_selected_rows <- reactiveVal(NULL)
-      observe({
-        channels_selected_rows(input$wizard_channels_table_rows_selected)
-      })
-      
-      # Runs server ----
-      
-      # Reactive variables for the runs
-      
-      # Vector of unique runs from raw data
-      wizard_runs <- reactive({
-        switch(input$platform,
-               PD = {
-                 raw()$Run
-               },
-               MQ = {
-                 if (length(raw()$Raw.file) != 0) {
-                   raw()$Raw.file
-                 } else {
-                   raw()$`Raw file`
-                 }
-               }
-        ) %>%
-          unique() %>%
-          sort()
-      })
-      
-      # Initialise blank columns for runs data frame
-      wizard_runs_mixtures <- reactiveVal(rep(NA, length(wizard_runs())))
-      wizard_fractions <- reactiveVal(NA)
-      wizard_techrepmixtures <- reactiveVal(NA)
-
-      # Update wizard_runs_data
-      observe({
-        wizard_runs_data(
-          data.frame(
-            Fraction = wizard_fractions(),
-            TechRepMixture = wizard_techrepmixtures()
-          )
-        )
-      })
-
-      # Render the run level data frame as a DataTable
-      output$wizard_runs_table <- DT::renderDataTable({
-        datatable(
-          wizard_runs_data(),
-          options = list(
-            dom = "t",
-            paging = FALSE,
-            ordering = FALSE
-          ),
-          rownames = FALSE,
-          editable = TRUE,
-          class = "cell-border stripe"
-        )
-      })
-
-      # Variable for selected rows, for runs and channels
-      runs_selected_rows <- reactiveVal(NULL)
-      observe({
-        runs_selected_rows(input$wizard_runs_table_rows_selected)
-      })
-
-      # Handler to edit Fraction if adding fractions manually
-      observeEvent(input$addFraction, {
-        if (!is.null(runs_selected_rows())) {
-          current_wizard_fractions <- wizard_fractions()
-          current_wizard_fractions[runs_selected_rows()] <- input$wizardFraction
-          wizard_fractions(current_wizard_fractions)
-        } else {
-          # Handle the case when no rows are selected
-          showNotification(
-            "Please select one or more rows first",
-            type = "error",
-            duration = NULL,
-            closeButton = TRUE
-          )
-        }
-      })
-
-      # Handler to edit Fraction if not fractionated
-      observeEvent(input$wizardFractionated, {
-        if (input$wizardFractionated) {
-          wizard_fractions(1)
-        } else {
-          wizard_fractions(rep(NA, length(wizard_runs())))
-        }
-      })
-
-      # Handler to edit TechRepMixture if adding replicates manually
-      observeEvent(input$addReplicate, {
-        if (!is.null(runs_selected_rows())) {
-          current_wizard_techrepmixtures <- wizard_techrepmixtures()
-          current_wizard_techrepmixtures[runs_selected_rows()] <- input$wizardTechRepMixture
-          wizard_techrepmixtures(current_wizard_techrepmixtures)
-        } else {
-          # Handle the case when no rows are selected
-          showNotification(
-            "Please select one or more rows first",
-            type = "error",
-            duration = NULL,
-            closeButton = TRUE
-          )
-        }
-      })
-
-      # Handler to edit TechRepMixture if not replicated
-      observeEvent(input$wizardReplicated, {
-        if (input$wizardReplicated) {
-          wizard_techrepmixtures(1)
-        } else {
-          wizard_techrepmixtures(rep(NA, length(wizard_runs())))
-        }
-      })
-
-      # Run the modal ----
       # Event handler to change the page
       wizard_page <- reactiveVal(1)
       observeEvent(input$nextButton, {
@@ -221,7 +216,7 @@ observeEvent(input$launch_wizard, {
       observeEvent(input$backButton, {
         wizard_page(wizard_page() - 1)
       })
-      
+
       # Event handler to close the modal with the "Done" button
       observeEvent(input$doneButton, {
         removeModal()
