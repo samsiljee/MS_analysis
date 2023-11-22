0675a8b349004d8d9543185f774713d77d4a4500
samsiljee <samsiljee@gmail.com>
2023-11-22 13:20:25 +1300

    Added small script to trim datasets for testing

---
+++ trim_datasets.R
@@ -0,0 +1,40 @@
+# Script to abbreviate datasets, making them faster for testing
+# Sam Siljee - 21 November 2023
+
+# Libraries
+library(vroom)
+library(dplyr)
+
+# Load the dataset
+dat <- vroom("input/DIANN/report.tsv")
+
+# Identify proteins present in all runs
+summary <- dat %>%
+    group_by(Protein.Group) %>%
+    summarise(Protein = Protein.Group, Number = length(unique(Run)))
+eligible_proteins <- summary %>%
+    filter(Number == 6) %>%
+    .$Protein.Group %>%
+    unique()
+filtered_dat <- dat %>%
+    filter(Protein.Group %in% eligible_proteins)
+
+# Identify list of most abundant proteins - for each run
+proteins <- NULL
+for(i in unique(filtered_dat$Run)) {
+    new_proteins <- filtered_dat %>%
+        filter(Run == i) %>%
+        arrange(Genes.MaxLFQ.Unique) %>%
+        .$Protein.Group %>%
+        unique() %>%
+        head(10)
+    proteins <- c(proteins, new_proteins)
+}
+
+
+# Filter data
+dat <- dat %>%
+    filter(Protein.Group %in% proteins)
+
+# Write table
+vroom_write(dat, "input/DIANN/report_truncated.tsv")
